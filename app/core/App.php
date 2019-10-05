22035560a918c4fa75e52dff2c346d89a58a0732
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-05 15:47:22 +0700

    mengelola url agar rapih

--- app/core/App.php
+++ app/core/App.php
@@ -3,7 +3,16 @@
 class APP {
 	public function __construct()
 	{
-		echo 'OK!';
+		$url = $this->parseURL();
+		var_dump($_GET);
+	}
+
+	public function parseURL()
+	{
+		if ( isset($_GET["url"]) ) {
+			$url = $_GET["url"];
+			return $url ;
+		}
 	}
 	
 }
