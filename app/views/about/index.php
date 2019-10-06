59b3a2e80365d484a8e92a9e349310790e799f65
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-06 22:33:15 +0700

    membuat jumbotron

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,3 +1,6 @@
+<div class="container">
 
-	<h1>About Me</h1>
+	<h1 class="mt-4">About Me</h1>
 	<p>Hallo, nama saya <?= $data["nama"]; ?>, umur saya <?= $data["umur"]; ?> tahun, saya adalah seorang <?= $data["pekerjaan"]; ?>. </p>
+
+</div>
