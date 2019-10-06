490096bb09f771ddd96a94afd986fb7140cdfefd
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-06 13:47:57 +0700

    menjalankan controller & method, serta kirimkan params jika ada

--- app/core/App.php
+++ app/core/App.php
@@ -28,9 +28,11 @@ class App {
 
 		//params
 		if ( !empty($url) ) {
-			var_dump($url);
+			$this->params = array_values($url);
 		}
 
+		// jalankan controller & method, serta kirimkan params jika ada 
+		call_user_func_array([$this->controller, $this->method], $this->params );
 
 	}
 
