619a09130fa7c4532bfd9e882888efe46efd0506
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-07 19:15:16 +0700

    mengelola data baru, dan lansung terisi ke database

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -10,7 +10,6 @@ class Mahasiswa extends Controller {
 		$this->view('templates/footer');
 
 	}
-
 		public function detail ($id)
 	{
 		$data['judul'] = 'Detail Mahasiswa'; 
@@ -18,6 +17,16 @@ class Mahasiswa extends Controller {
 		$this->view('templates/header', $data);
 		$this->view('mahasiswa/detail', $data);
 		$this->view('templates/footer');
-
 	}
+
+	public function tambah()
+	{
+		if( $this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0){
+			header('Location: ' . BASEURL . '/mahasiswa');
+			exit;
+		}
+	}
+
+
+
 }
--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -64,7 +64,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Tambah Data</button>
+        <button type="submit" class="btn btn-primary">Tambah Data</button>
 		</form>
       </div>
     </div>
