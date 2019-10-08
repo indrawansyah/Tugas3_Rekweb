9dd1d73fd5e4edf17b5be360594c2a6f2dbd8233
Indrawansyah Prasetyo <indrawnsyhprstyo@gmail.com>
2019-10-08 18:07:51 +0700

    menambahkan tombol ubah

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -18,6 +18,7 @@
 			  <li class="list-group-item">
 			  	<?= $mhs['nama'] ?>
 			  	<a href= "<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'];  ?>" class="badge badge-danger float-right ml-1" onclick="return confirm('yakin?');" >Hapus</a>	
+				<a href= "<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id'];  ?>" class="badge badge-success float-right ml-1" data-toggle="modal" data-target="#formModal">ubah</a>
 			  	<a href= "<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'];  ?>" class="badge badge-primary float-right ml-1">Detail</a>	
 			  	</li>
 			  <?php endforeach; ?>
